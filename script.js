eec5e5a3f9d4dbfcd6d312c02146bd1c8c35a0fb
guimoouraa <guilherme.mouraa@outlook.com>
2024-08-07 00:36:08 -0300

    Added fuction for includ tasks itens

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let minhaListaDeItens = []
 function adicionarNovaTarefa() {
     minhaListaDeItens.push(input.value)
 
+    input.value = ''
+
     mostrarTarefas()
 }
 
@@ -14,17 +16,23 @@ function mostrarTarefas() {
 
     let novaLi = ''
 
-    minhaListaDeItens.forEach( (tarefa) => {
+    minhaListaDeItens.forEach( (tarefa, posicao) => {
         novaLi = novaLi + `
               <li class="task">
                          <img src="img/checked.png" alt="">
                          <p>${tarefa}</p>
-                         <img src="img/trash.png" alt="">
+                         <img src="img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
               </li>
         `
     })
 
     listaCompleta.innerHTML = novaLi
+}
+
+function deletarItem(posicao){
+    minhaListaDeItens.splice(posicao, 1)
+
+    mostrarTarefas()
 }
 
 button.addEventListener('click', adicionarNovaTarefa)
